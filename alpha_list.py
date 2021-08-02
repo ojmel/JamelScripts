96807a7895d599cb4fa2463b3c10e29d6cd20e75
ojmel <61952149+ojmel@users.noreply.github.com>
2021-08-02 13:36:42 -0400

    Add files via upload

---
+++ alpha_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#SBATCH -p gpu          # partition
+#SBATCH --gres=gpu:v100:1    # number of GPUs
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 8            # number of cores
+#SBATCH -t 50:00:00     # time
+
+import argparse
+import os
+
+def run_alpha(seqfile, outputdir):
+  """Wrapper to run AlphaFold on one sequence.
+  Args:
+    seqfile: full path to FASTA file
+    outputdir:  full path for output directory."""
+  os.system('/scratch/jws6pq/Notebook/Finished/alpha_run.sh %s %s' % (seqfile, outputdir))
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', help='tab-separated list of sequence files',
+                     type=str)
+  parser.add_argument('-o', help='output directory', type=str)
+  args = parser.parse_args()
+  seqlist = args.s.split('\n')
+  for seq in seqlist:
+    run_alpha(os.path.abspath(seq), os.path.abspath(args.o))
---
+++ alpha_run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH -p gpu          # partition
+#SBATCH --gres=gpu:v100:2    # number of GPUs
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 16            # number of cores
+#SBATCH -t 20:00:00     # time
+
+# IMPORTANT
+# run this with input FASTA and output full paths as $1 and $2
+export FASTA=$1
+export ALPHA_OUT=$2
+export MODEL_NAMES=$Poop
+# use preset = full_dbs or casp14
+export MODE=full_dbs
+
+export TEMPLATE_LIMIT=2021-12-12
+
+module purge
+module load singularity alphafold
+
+export TF_FORCE_UNIFIED_MEMORY=1
+export XLA_PYTHON_CLIENT_ALLOCATOR=platform
+
+singularity run -B $ALPHAFOLD_DATA_PATH:/data -B .:/etc --pwd /app/alphafold --nv $CONTAINERDIR/alphafold-2.0.0.sif \
+    --fasta_paths=$FASTA \
+    --output_dir=$ALPHA_OUT \
+    --model_names=$MODEL_NAMES \
+    --preset=$MODE \
+    --max_template_date=$TEMPLATE_LIMIT \
+    --data_dir=/data \
+    --uniref90_database_path=/data/uniref90/uniref90.fasta \
+    --mgnify_database_path=/data/mgnify/mgy_clusters.fa \
+    --uniclust30_database_path=/data/uniclust30/uniclust30_2018_08/uniclust30_2018_08 \
+    --bfd_database_path=/data/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
+    --pdb70_database_path=/data/pdb70/pdb70 \
+    --template_mmcif_dir=/data/pdb_mmcif/mmcif_files \
+    --obsolete_pdbs_path=/data/pdb_mmcif/obsolete.dat
