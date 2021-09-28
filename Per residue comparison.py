8b76af36e4ef80a6a175e35087427d8ab021f5aa
ojmel <61952149+ojmel@users.noreply.github.com>
2021-09-28 13:03:22 -0400

    Add files via upload

--- Per residue comparison.py
+++ Per residue comparison.py
@@ -1,22 +1,27 @@
 import math
 import numpy as np
 k=1
-file5=open('ProteinList.tsv','r')
+file5=open('SARS2-MurineA59RightProteinList.tsv','r')
+Title='E:\Research\\' + file5.name.replace('ProteinList.tsv', 'PercentDifference.tsv')
+Title2='E:\Research\\' + file5.name.replace('ProteinList.tsv', 'RBDPercentDifference.tsv')
 ProteinList=file5.readlines()
 l=0
 j=0
 h=0
 p=0
+z=0
 Quadrupletsperlist=int(len(ProteinList)/4)
-PercentDifference=np.empty((len(ProteinList),2), dtype=object)
+PercentDifference=np.empty(((int(len(ProteinList)/4)),4), dtype=object)
+PercentDifferenceRBD=np.empty(((int(len(ProteinList)/4)),4), dtype=object)
 Plddtfiles=np.empty((len(ProteinList)), dtype=object)
 for line in ProteinList:
     x=line.split()
     Plddtfiles[l]=''
     Plddtfiles[l]=x[0]
     l+=1
-for h in range(len(ProteinList)):
+for h in range(int(len(ProteinList)/4)):
     PercentDifference[h]=''
+    PercentDifferenceRBD[h]=''
     h+=1
 for k in range(Quadrupletsperlist):
     file=open(Plddtfiles[0+j],"r")
@@ -27,120 +32,108 @@ for k in range(Quadrupletsperlist):
     Score2=file2.readlines()
     Score3=file3.readlines()
     Score4=file4.readlines()
-    FirstSplice=319/1273
-    SecondSplice=541/1273
     LengthofProtein1=len(Score1)
     LengthofProtein2=len(Score2)
     LengthofChimera1=len(Score3)
-    BeforeSpliceLengthforChimera1=int(FirstSplice*LengthofProtein1)
-    SpliceLengthofChimera1=int(SecondSplice*LengthofProtein2-FirstSplice*LengthofProtein2)
-    AfterSpliceLengthofChimera1=LengthofChimera1-BeforeSpliceLengthforChimera1-SpliceLengthofChimera1
-
     i=0
     Sumofnonsplice1=0
     Sumofsplice1=0
     for i in range(LengthofChimera1):
-        if i<BeforeSpliceLengthforChimera1:
+
+        if i<(int(ProteinList[z+2].split()[1])):
             Sumofnonsplice1+=float(Score3[i])
             i+=1
-        elif (SpliceLengthofChimera1+BeforeSpliceLengthforChimera1)>i>=BeforeSpliceLengthforChimera1:
+        elif (int(ProteinList[z+2].split()[2])+int(ProteinList[z+2].split()[1]))>i>=int(ProteinList[z+2].split()[1]):
             Sumofsplice1+=float(Score3[i])
             i+=1
-        elif i>= (SpliceLengthofChimera1+BeforeSpliceLengthforChimera1):
+        elif i>= (int(ProteinList[z+2].split()[1])+int(ProteinList[z+2].split()[2])):
             Sumofnonsplice1+=float(Score3[i])
             i+=1
-    Averageofsplice1=(Sumofsplice1/SpliceLengthofChimera1)
-    Averageofnonsplice1=(Sumofnonsplice1/(LengthofChimera1-SpliceLengthofChimera1))
+    Averageofsplice1=(Sumofsplice1/int(ProteinList[z+2].split()[2]))
+    Averageofnonsplice1=(Sumofnonsplice1/(LengthofChimera1-int(ProteinList[z+2].split()[2])))
 
     LengthofChimera2=len(Score4)
-    BeforeSpliceLengthforChimera2=int(FirstSplice*LengthofProtein2)
-    SpliceLengthofChimera2=int(SecondSplice*LengthofProtein1-FirstSplice*LengthofProtein1)
-    AfterSpliceLengthofChimera2=LengthofChimera2-BeforeSpliceLengthforChimera2-SpliceLengthofChimera2
     i=0
     Sumofnonsplice2=0
     Sumofsplice2=0
     for i in range(LengthofChimera2):
-        if i<BeforeSpliceLengthforChimera2:
+        if i<int(ProteinList[z+3].split()[1]):
             Sumofnonsplice2+=float(Score4[i])
             i+=1
-        elif (SpliceLengthofChimera2+BeforeSpliceLengthforChimera2)>i>=BeforeSpliceLengthforChimera2:
+        elif (int(ProteinList[z+3].split()[2])+int(ProteinList[z+3].split()[1]))>i>=int(ProteinList[z+3].split()[1]):
             Sumofsplice2+=float(Score4[i])
             i+=1
-        elif i>= (SpliceLengthofChimera2+BeforeSpliceLengthforChimera2):
+            print(i)
+        elif i>= (int(ProteinList[z+3].split()[2])+int(ProteinList[z+3].split()[1])):
             Sumofnonsplice2+=float(Score4[i])
             i+=1
-    Averageofsplice2=(Sumofsplice2/SpliceLengthofChimera2)
-    Averageofnonsplice2=(Sumofnonsplice2/(LengthofChimera2-SpliceLengthofChimera2))
+    Averageofsplice2=(Sumofsplice2/int(ProteinList[z+3].split()[2]))
+    Averageofnonsplice2=(Sumofnonsplice2/(LengthofChimera2-int(ProteinList[z+3].split()[2])))
+
 
 
     i=0
     Sumofnonrbd2=0
     Sumofrbd2=0
     for i in range(LengthofProtein2):
-        if i<BeforeSpliceLengthforChimera2:
+        if i<int(ProteinList[z+1].split()[1]):
             Sumofnonrbd2+=float(Score2[i])
             i+=1
-        elif (SpliceLengthofChimera1+BeforeSpliceLengthforChimera2)>i>=BeforeSpliceLengthforChimera2:
+        elif (int(ProteinList[z+1].split()[1])+int(ProteinList[z+1].split()[2]))>i>=int(ProteinList[z+1].split()[1]):
             Sumofrbd2+=float(Score2[i])
+
             i+=1
-        elif i>= (SpliceLengthofChimera1+BeforeSpliceLengthforChimera2):
+        elif i>= (int(ProteinList[z+1].split()[2])+int(ProteinList[z+1].split()[1])):
             Sumofnonrbd2+=float(Score2[i])
             i+=1
-    Averageofrbd2=(Sumofrbd2/SpliceLengthofChimera1)
-    Averageofnonrbd2=(Sumofnonrbd2/(LengthofProtein2-SpliceLengthofChimera1))
+    Averageofrbd2=(Sumofrbd2/int(ProteinList[z+1].split()[2]))
+    Averageofnonrbd2=(Sumofnonrbd2/(LengthofProtein2-int(ProteinList[z+2].split()[2])))
 
 
     i=0
     Sumofnonrbd1=0
     Sumofrbd1=0
     for i in range(LengthofProtein1):
-        if i<BeforeSpliceLengthforChimera1:
+        if i<int(ProteinList[z].split()[1]):
             Sumofnonrbd1+=float(Score1[i])
             i+=1
-        elif (SpliceLengthofChimera2+BeforeSpliceLengthforChimera1)>i>=BeforeSpliceLengthforChimera1:
+        elif (int(ProteinList[z].split()[2])+int(ProteinList[z].split()[1]))>i>=int(ProteinList[z].split()[1]):
             Sumofrbd1+=float(Score1[i])
+
+
             i+=1
-        elif i>= (SpliceLengthofChimera1+BeforeSpliceLengthforChimera1):
+        elif i>= (int(ProteinList[z].split()[2])+int(ProteinList[z].split()[1])):
             Sumofnonrbd1+=float(Score1[i])
             i+=1
-    Averageofrbd1=(Sumofrbd1/SpliceLengthofChimera2)
-    Averageofnonrbd1=(Sumofnonrbd1/(LengthofProtein1-SpliceLengthofChimera1))
-
-    #PercentError=np.zeros((len(Score1),2))
-
-    #print(Score1[1])
-    #Lengthofover10=0
-    #for i in range(len(Score1)):
-        #PercentError[i,0]=((math.fabs(float(Score1[i])-float(Score3[i])))/float(Score1[i]))*100
-        #PercentError[i,1]=int(i+1)
-        #if PercentError[i,0]>10:
-            #Lengthofover10+=1
-    #print(Lengthofover10)
-    #PercentErrorover10=np.zeros((Lengthofover10,2))
-    #i=0
-    #j=0
-    #for j in range(len(Score1)):
-        #if PercentError[j,0]>10:
-            #PercentErrorover10[i,0]=PercentError[j,0]
-            #PercentErrorover10[i, 1] = PercentError[j, 1]
-            #i+=1
+    #how can i name this better with different situations?
+    Averageofrbd1=(Sumofrbd1/int(ProteinList[z].split()[2]))
+    Averageofnonrbd1=(Sumofnonrbd1/(LengthofProtein1-int(ProteinList[z].split()[2])))
     Chimera1 = file3.name.replace('RBD.plddt', 'NonRBD')
     Chimera1RBD = file3.name.replace('RBD.plddt', 'RBD')
     Chimera2 = file4.name.replace('RBD.plddt', 'NonRBD')
     Chimera2RBD = file4.name.replace('RBD.plddt', 'RBD')
+    Chimera1 = Chimera1.replace('E:\\Plddt\\', '')
+    Chimera1RBD = Chimera1RBD.replace('E:\\Plddt\\', '')
+    Chimera2 = Chimera2.replace('E:\\Plddt\\', '')
+    Chimera2RBD = Chimera2RBD.replace('E:\\Plddt\\', '')
+    print(Averageofsplice2)
+    print(Averageofrbd1)
     DifferenceofNonRBD1Averages=((Averageofnonrbd1-Averageofnonsplice1)/Averageofnonrbd1)*100
-    DifferenceofRBD1Averages=((Averageofrbd1-Averageofsplice2)/Averageofrbd1)*100
+    DifferenceofRBD1Averages=((Averageofrbd2-Averageofsplice1)/Averageofrbd2)*100
     DifferenceofNonRBD2Averages=((Averageofnonrbd2-Averageofnonsplice2)/Averageofnonrbd2)*100
-    DifferenceofRBD2Averages=((Averageofrbd2-Averageofsplice1)/Averageofrbd2)*100
+    DifferenceofRBD2Averages=((Averageofrbd1-Averageofsplice2)/Averageofrbd1)*100
     PercentDifference[p,0]=Chimera1
     PercentDifference[p,1]=DifferenceofNonRBD1Averages
-    PercentDifference[p+1, 0] = Chimera1RBD
-    PercentDifference[p+1, 1] = DifferenceofRBD1Averages
-    PercentDifference[p + 2, 0] = Chimera2
-    PercentDifference[p + 2, 1] = DifferenceofNonRBD2Averages
-    PercentDifference[p + 3, 0] = Chimera2RBD
-    PercentDifference[p + 3, 1] = DifferenceofRBD2Averages
+    PercentDifferenceRBD[p, 0] = Chimera1RBD
+    PercentDifferenceRBD[p, 1] = DifferenceofRBD1Averages
+    PercentDifference[p, 2] = Chimera2
+    PercentDifference[p, 3] = DifferenceofNonRBD2Averages
+    PercentDifferenceRBD[p, 2] = Chimera2RBD
+    PercentDifferenceRBD[p, 3] = DifferenceofRBD2Averages
     j += 4
-    p+=4
-print(PercentDifference)
-np.savetxt('AveragePerResiduePercentDifferencesRightShift.tsv',PercentDifference,fmt="%s,%s",delimiter=" ")
+    p+=1
+    z+=4
+print(PercentDifferenceRBD)
+
+np.savetxt(Title,PercentDifference,fmt="%s,%s,%s,%s",delimiter=" ")
+np.savetxt(Title2,PercentDifferenceRBD,fmt="%s,%s,%s,%s",delimiter=" ")
--- RBDExchanger.py
+++ RBDExchanger.py
@@ -1,17 +1,19 @@
 import numpy as np
 
-file=open('SARS.fasta',"r")
-file2=open('MurineA59.fasta',"r")
+file=open('AvianD274.fasta',"r")
+file2=open('SARS2.fasta',"r")
 Fasta1=file.readlines()
 Fasta2=file2.readlines()
 Protein1=file.name.replace('.fasta','')
 Protein2=file2.name.replace('.fasta','')
-Title=Protein1+'w'+Protein2+'RBD.fasta'
-Title2=Protein2+'w'+Protein1+'RBD.fasta'
-#Default is 331
-FirstSplice=319
-#Sefault is 524
-SecondSplice=541
+Title=Protein1+'w'+Protein2+'_TruncS1.fasta'
+Path='E:\Fastas\\'  +Title
+Title2=Protein2+'w'+Protein1+'_TruncS1.fasta'
+Path2='E:\Fastas\\'  +Title2
+#Default is 319
+FirstSplice=306
+#Sefault is 541
+SecondSplice=685
 Sequence1=''
 Sequence2=''
 for i in range(len(Fasta1)-1):
@@ -21,6 +23,8 @@ for i in range(len(Fasta2)-1):
 Seqlength=len(Sequence1)
 Seqlength2=len(Sequence2)
 Sections=np.empty((3,2), dtype=object)
+ProteinList=np.empty((4,3), dtype=object)
+
 print(Sequence1)
 print(Sequence2)
 Sections[0,0]=''
@@ -30,7 +34,7 @@ i=0
 j=0
 for letter in Sequence1:
     i+=1
-    if i<=Seqlength*(FirstSplice/1273):
+    if i<Seqlength*(FirstSplice/1273):
         Sections[0,0]+=str(letter)
     elif Seqlength*(FirstSplice/1273)<i<=Seqlength*(SecondSplice/1273):
         Sections[1,0] += str(letter)
@@ -43,7 +47,7 @@ i=0
 j=1
 for letter in Sequence2:
     i+=1
-    if i<=Seqlength2*(FirstSplice/1273):
+    if i<Seqlength2*(FirstSplice/1273):
         Sections[0,1]+=str(letter)
     elif Seqlength2*(FirstSplice/1273)<i<=Seqlength2*(SecondSplice/1273):
         Sections[1,1] += str(letter)
@@ -52,13 +56,24 @@ for letter in Sequence2:
 NewSequence=np.empty((2,1), dtype=object)
 NewSequence[0,0]='>Chimera'
 NewSequence[1,0]=Sections[0,0]+Sections[1,1]+Sections[2,0]
-print(Sections)
+m=0
 
-np.savetxt(Title,NewSequence,fmt="%s",delimiter=" ")
+np.savetxt(Path,NewSequence,fmt="%s",delimiter=" ")
 NewSequence2=np.empty((2,1), dtype=object)
 NewSequence2[0,0]='>Chimera'
 NewSequence2[1,0]=Sections[0,1]+Sections[1,0]+Sections[2,1]
-#print(NewSequence2)
-np.savetxt(Title2,NewSequence2,fmt="%s",delimiter=" ")
-
+np.savetxt(Path2,NewSequence2,fmt="%s",delimiter=" ")
+ProteinList[m,0]=Protein1 + '.plddt'
+ProteinList[m+1,0] =Protein2 + '.plddt'
+ProteinList[m+2,0] = 'E:\Plddt\\' +Protein1 +'w'+Protein2+ '_'+'TruncS1.plddt'
+ProteinList[m+3,0] = 'E:\Plddt\\' +Protein2+'w'+Protein1+ '_'+'TruncS1.plddt'
+ProteinList[m, 1]  = ProteinList[m+2,1]=len(Sections[0,0])
+ProteinList[m+1, 1] = ProteinList[m + 3, 1] =len(Sections[0, 1])
+ProteinList[m, 2] =ProteinList[m+3,2]=len(Sections[1, 0])
+ProteinList[m + 2, 2] =ProteinList[m+1,2]=len(Sections[1, 1])
+np.savetxt('SProteinList.tsv', ProteinList, fmt="%s", delimiter=" ")
+print(ProteinList)
+AlphaFoldEntry=''
+AlphaFoldEntry+= "/scratch/jws6pq/Notebook/Finished/" + Title +',' +"/scratch/jws6pq/Notebook/Finished/" + Title2 + ','
+print(AlphaFoldEntry)
 exit()
