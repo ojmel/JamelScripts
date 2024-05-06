a654462747b9a97b5d6214d484a23e60104802e4
ojmel <jamelsmpsn@gmail.com>
2024-05-06 10:47:26 -0400

    h

--- PCA.py
+++ PCA.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 tcga=pd.read_csv('tcga_hpv_feature_matrix.csv')
 # center points on origin
 # make lines intersecting origin and find largest distance of projections for all points
@@ -7,5 +8,10 @@ tcga=pd.read_csv('tcga_hpv_feature_matrix.csv')
 #average ofsum of squares is eigenvalue or variation
 #pc2 is perpendicular to
 tcga=tcga.drop(columns=['patient','Unnamed: 0','hpv_status'])
-print(tcga)
-print(tcga @ tcga.transpose())
+tcga=np.array(tcga)
+def covariance_matrix(matrix):
+    matrix_mean=np.mean(matrix,axis=0)
+    centered_matrix=matrix-matrix_mean
+    squared_matrix=centered_matrix * centered_matrix
+    variance_vector=np.sum(squared_matrix,axis=0)/centered_matrix.shape[0]
+covariance_matrix(tcga)
